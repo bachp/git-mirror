58ab39567ebd5c42ded46417915d491e452a13f2
Pascal Bach <pascal.bach@nextrem.ch>
2025-01-24 21:09:15 +0100

    prepare release 0.14.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Changed
+
+## [0.14.13] - 2025-01-24
+
+- Update dependencies
+
 ## [0.14.12] - 2023-02-08
 
 ### Changed
--- src/main.rs
+++ src/main.rs
@@ -145,7 +145,11 @@ fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or(env_log_level)).init();
 
     // Run OpenSSL probing on all platforms even the ones not using it
-    openssl_probe::init_ssl_cert_env_vars();
+    // TODO: Switch to rustls-platform-verifier and rustls once it is supported by
+    // reqwest https://github.com/seanmonstar/reqwest/issues/2159
+    unsafe {
+        openssl_probe::init_openssl_env_vars();
+    };
 
     let provider: Box<dyn Provider> = match opt.provider {
         Providers::GitLab => Box::new(GitLab {
