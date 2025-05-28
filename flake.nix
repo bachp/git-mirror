a6a885c907e0b23879c7802846e3f832f4754553
Pascal Bach <pascal.bach@nextrem.ch>
2025-05-28 17:48:06 +0200

    add flake for nix develop and nix build

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A flake for building the git-mirror project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      nativeBuildInputs = with pkgs; [
+          cargo
+          gcc
+          pkg-config
+          perl
+      ];
+      buildInputs = with pkgs; [
+          openssl.dev
+          openssl
+      ];
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        inherit buildInputs;
+        inherit nativeBuildInputs;
+
+        shellHook = ''
+          echo "Welcome to the git-mirror development environment!"
+        '';
+      };
+
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        inherit buildInputs;
+        inherit nativeBuildInputs;
+
+        pname = cargoToml.package.name;
+        version = cargoToml.package.version;
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+      };
+    };
+}
